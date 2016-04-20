d3e7e8c6172c64d36d05a043b9239684a333f003
root <root@raspberrypi.(none)>
2016-04-20 21:50:11 +0000

    Implementado desactivar camara y placa

--- Servidor/Flaskr/flaskr/flaskr.py
+++ Servidor/Flaskr/flaskr/flaskr.py
@@ -1,11 +1,12 @@
 # all the imports
+import servidorConf
 import threading
-import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
 	 abort, render_template, flash, Response
 from contextlib import closing
 import serial
 import subprocess
+import os
 
 from camera import VideoCamera
 # configuration
@@ -20,10 +21,24 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'some_really_long_random_string_here'
 logged= None
-ser = serial.Serial('/dev/ttyACM0', 9600)
+if servidorConf.board==1:
+	ser = serial.Serial('/dev/ttyACM0', 9600)
+else:	
+	print 'Controller board is not activated'
+def cameraServer():
+	os.chdir("/home/trex/TFG/mjpg/mjpg-streamer/")
+        print('Video streaming starting on pid',  os.getpid())
+        os.system('/home/trex/TFG/mjpg/mjpg-streamer/mjpg_streamer -i "./input_uvc.so -d /dev/video0" -o "./output_http.so -w ./www" ')
+
+if servidorConf.camera==1:
+	newPid=os.fork()
+	if newPid==0:
+		cameraServer()	
 def send(message,ser):
-	message='&'+message+'#'
-	ser.write(message)
+	if servidorConf.board==1:
+		message='&'+message+'#'
+		ser.write(message)
+
 
 @app.route('/command', methods=['GET','POST'])
 def command():
@@ -65,6 +80,7 @@ def logout():
 	#session.pop('logged_in', None)
 	flash('You were logged out')
 	return redirect(url_for('show_entries'))
+  
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=4000)    
