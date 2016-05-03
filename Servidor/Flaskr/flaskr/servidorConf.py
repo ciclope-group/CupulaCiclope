81ef4bfbc32fc8096d6543f2166c6cc543602950
plozano94 <plozano94@gmail.com>
2016-05-03 23:21:54 +0000

    Avances

--- Servidor/Flaskr/flaskr/servidorConf.py
+++ Servidor/Flaskr/flaskr/servidorConf.py
@@ -1,5 +1,5 @@
-camera=0
-board=0
+camera=1
+board=1
 boardPort='/dev/ttyACM0'
 logDir="Log/log.txt"
 mjpgDir= "/home/trex/TFG/CupulaCiclope/Servidor/mjpg/mjpg-streamer/"
--- Servidor/serverInit.sh
+++ Servidor/serverInit.sh
@@ -1,1 +1,2 @@
+#! /bin/bash
 sudo Flaskr/flaskr/flaskr.py
