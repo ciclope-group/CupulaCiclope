ed5ce273b1f010baef0b1fd692ca816a4d2259e2
Pablo Lozano <plozano94@gmail.com>
2017-01-12 11:47:56 +0100

    Reestructuración

--- RabbitMQ/Cola/config.py
+++ RabbitMQ/Cola/config.py
@@ -1,4 +1,4 @@
 me = "cupula"
 list = ["meteor","telescopio"]
-urlServer="venus.datsi.fi.upm.es"
+urlServer="xxx"
 severity=["critical","info"]
---
+++ Servidor/serverInit.sh~
@@ -0,0 +1,2 @@
+#! /bin/bash
+sudo Flaskr/flaskr/flaskr2.py
